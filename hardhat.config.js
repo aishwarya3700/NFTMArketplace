50d7909ef1ea80f4980627af70baccfe1e64e99f
Aishwarya <100249559+aishwarya3700@users.noreply.github.com>
2023-05-31 21:08:59 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://polygon-mumbai.g.alchemy.com/v2/kQ_6c7eLQp0TWDhoe__ULXGTf4E13iht";
+const NEXT_PUBLIC_PRIVATE_KEY = "15f3c050e5b49f230c436508e00718b0d2d4257b339a460e269b3510575f22d2";
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  solidity: "0.8.17",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    polygon_mumbai: {
+      url: NEXT_PUBLIC_POLYGON_MUMBAI_RPC,
+      accounts: [`0x${NEXT_PUBLIC_PRIVATE_KEY}`],
+    },
+  },
+
+  //   // fuji: {
+  //   //   url: `Your URL`,
+  //   //   accounts: [
+  //   //     `0x${"Your Account"}`,
+  //   //   ],
+  //   // },
+  // },
+};
